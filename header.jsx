00cfdc43f02e9842eddb92790994a732b5260d04
nandirb <58172661+nandirb@users.noreply.github.com>
2020-10-24 12:41:03 +0800

    Add files via upload

---
+++ header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './header.css';
+
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div className="main-header">
+                <nav>
+                    <ul>
+                    <li className="items"></li>
+                    <li className="items"></li>
+                    <li className="items"></li>
+                    <li className="items"></li>
+                    </ul>
+                </nav>
+                   
+                
+            </div>
+        )
+    }
+}
+export default Header;
